522e983f7d8586e003926ca4bc1621269941d064
qStentyq <stenty2s@gmail.com>
2024-07-17 16:43:56 +0300

    onewayusdcurs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+    const [blockValue, setBlockValue] = useState(0);
+    const [apiResponse, setAPIResponse] = useState({});
+    const [currency, setCurrency] = useState("RUB");
+    useEffect(() => {
+        fetch("https://open.er-api.com/v6/latest/USD")
+            .then((resp) => resp.json())
+            .then((json) => {
+                setAPIResponse(json.rates);
+                console.log(json.rates);
+            });
+    }, []);
+
+    const onChangeCurrency = (cur) => {
+        setCurrency(cur);
+    };
+    return (
+        <div className='App'>
+            <Block
+                value={blockValue}
+                onChangeValue={setBlockValue}
+                currency='USD'
+                onChangeCurrency={(cur) => console.log(cur)}
+            />
+            <Block value={apiResponse[currency] * blockValue} currency={currency} onChangeCurrency={onChangeCurrency} />
+        </div>
+    );
 }
 
 export default App;
